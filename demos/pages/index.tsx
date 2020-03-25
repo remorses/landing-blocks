ac5d6bde0da3764ca1467dbfc817d3cc93e61c4a
remorses <beats.by.morse@gmail.com>
2020-03-25 18:40:03 +0100

    demos/p: namespaced prismic assets

--- demos/pages/index.tsx
+++ demos/pages/index.tsx
@@ -17,7 +17,7 @@ import {
 const Page = () => (
     <LandingProvider>
         <NavBar
-            logo={<img width='120px' src='logo.svg' />}
+            logo={<img width='120px' src='/prismic/logo.svg' />}
             navs={[
                 <a>Features</a>,
                 <a>Use Cases</a>,
@@ -30,13 +30,13 @@ const Page = () => (
             heading='Make your website editable for the whole team'
             subhead='Choose your technology. Use the API to fetch content. Empower
         your content team.'
-            image={<img width='500px' src='/code.png' />}
+            image={<img width='500px' src='/prismic/code.png' />}
             cta='Test it out'
         />
         <Feature
             heading='Prismic is a Content Management System, a tool for editing online content'
             subhead="Also known as a headless CMS, an API CMS, a content platform, a disruptive content-as-a-service digital experience...basically we've built a tool that lets you choose your technology, framework, and language and then easily manage your content."
-            image={<img src='feature1.jpg' width='500px' />}
+            image={<img src='/prismic/feature1.jpg' width='500px' />}
             backgroundColor='gray.100'
             flip
         />
@@ -46,21 +46,21 @@ const Page = () => (
             steps={[
                 {
                     heading: 'Model your editor',
-                    image: <img width='500px' src='step1.jpg' />,
+                    image: <img width='500px' src='/prismic/step1.jpg' />,
                     subhead:
                         "Model custom types to match your desired design. Create reusable fields and custom components (we call them Slices...you'll hear us talk about them a lot) to enable dynamic layouts and build the editor experience that you want",
                 },
                 {
                     heading:
                         'Hand over to your content team to start creating content',
-                    image: <img width='500px' src='step2.jpg' />,
+                    image: <img width='500px' src='/prismic/step2.jpg' />,
                     subhead:
                         "Once you've managed the initial setup, you can hand the management over to your content team so that editors can start filling content while you're choosing the framework. With Prismic, creating beautiful content becomes simple enough that even non-technical employees can handle it. ",
                 },
                 {
                     heading:
                         'Deploy on Zeit Now, Netlify, Heroku or your favorite hosting platform',
-                    image: <img width='500px' src='step3.png' />,
+                    image: <img width='500px' src='/prismic/step3.png' />,
                     subhead:
                         'We take care of hosting and scaling the publishing tools. All you have to do is host the website, but you can choose whichever hosting platform you prefer.',
                 },
