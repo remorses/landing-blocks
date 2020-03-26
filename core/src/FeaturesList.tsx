3e73616e22320535e218cb4f36a092eb19ffc7e9
remorses <beats.by.morse@gmail.com>
2020-03-26 01:29:29 +0100

    core/src/FeaturesList: fix

--- core/src/FeaturesList.tsx
+++ core/src/FeaturesList.tsx
@@ -29,9 +29,9 @@ export const FeaturesList = ({
                     {subhead}
                 </Text>
             </Stack>
-            <Stack isInline spacing={4} flex='1' flexWrap='wrap'>
+            <Stack flexDir='row' spacing='20px' flex='1' flexWrap='wrap'>
                 {features.map((step, i) => (
-                    <Feature key={i} {...step} />
+                    <Feature minW={{sm: '100%', lg: '0'}} flex='1' key={i} {...step} />
                 ))}
             </Stack>
         </PageContainer>
@@ -40,8 +40,8 @@ export const FeaturesList = ({
 
 const Feature = ({ heading, subhead, icon, ...rest }) => {
     return (
-        <Stack spacing='20px' p='40px' {...rest}>
-            <Box>{icon}</Box>
+        <Stack w='auto' spacing='20px' p='40px' {...rest}>
+            <Box >{icon}</Box>
             <Heading fontSize='20px'>{heading}</Heading>
             <Text m={0} fontSize='18px' opacity={0.6} lineHeight='30px'>
                 {subhead}
