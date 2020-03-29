b619928bb4ebdc57152c7440ef9fe2fb9383ac63
remorses <beats.by.morse@gmail.com>
2020-03-29 18:22:03 +0200

    made card options

---
+++ chakra-ui-forms/src/CardOptions.tsx
@@ -0,0 +1,103 @@
+import React, { FC, useContext, useEffect, Props } from 'react'
+import styled from '@emotion/styled'
+import {
+    Box,
+    FlexProps,
+    Stack,
+    Text,
+    Flex,
+    Button,
+    StackProps,
+    BoxProps,
+    ButtonProps,
+} from '@chakra-ui/core'
+import { useField, FieldProps, FieldInputProps } from 'react-final-form'
+
+export interface CardOptionsOption {
+    isDisabled?: boolean
+    label?: string
+    sublabel?: any
+    disabled?: boolean
+    value: any
+    icon?: any
+}
+
+function addDefaults(x: CardOptionsOption) {
+    const newoption = { ...x }
+    if (!x.label) {
+        newoption.label = x.value
+    }
+    return newoption
+}
+
+function Option(
+    props: {
+        option: CardOptionsOption
+        input: FieldInputProps<string>
+    } & Omit<ButtonProps, 'children'>,
+) {
+    let { option, input, ...rest } = props
+    option = addDefaults(option)
+    const onClick = () => {
+        input.onChange(option.value)
+    }
+    const selected = input.value === option.value
+    return (
+        <Button
+            border={selected ? '2px solid' : 'none'}
+            borderColor={selected ? 'gray.400' : 'gray.200'}
+            height='auto'
+            variant='outline'
+            onClick={onClick}
+            p='20px'
+            flex='1'
+            minW='100px'
+            d='flex'
+            flexDir='column'
+            justifyContent='flex-start'
+            alignItems='center'
+            // wordBreak='break-word'
+            whiteSpace='normal'
+            {...rest}
+        >
+            <Stack
+                minH='100%'
+                align='center'
+                justify='flex-start'
+                spacing='10px'
+            >
+                <Flex fontSize='32px' w='100%' align='center' justify='center'>
+                    {option.icon}
+                </Flex>
+                <Text fontSize='16px'>{option.label}</Text>
+                <Text opacity={0.6} fontSize='14px'>
+                    {option.sublabel}
+                </Text>
+                <Box flex='1' />
+            </Stack>
+        </Button>
+    )
+}
+
+export function CardOptions({
+    name,
+    options,
+    defaultValue = null,
+    ...rest
+}: {
+    name: string
+    defaultValue?: string
+    options: CardOptionsOption[]
+} & StackProps) {
+    const {
+        meta: { invalid },
+        input,
+    } = useField(name, { initialValue: defaultValue })
+    return (
+        <Stack spacing='20px' direction='row' {...rest}>
+            {options.map((option) => (
+                <Option key={option.value} option={option} input={input} />
+            ))}
+        </Stack>
+    )
+}
---
+++ chakra-ui-forms/stories/CardOptions.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { FiAirplay, FiAlertOctagon, FiAnchor } from 'react-icons/fi'
+import { CardOptions } from '../src/CardOptions'
+import { Form } from 'react-final-form'
+import { ThemeProvider, CSSReset, Box } from '@chakra-ui/core'
+
+export default {
+    title: 'CardOptions',
+    component: CardOptions,
+    decorators: [
+        (storyFn) => (
+            <ThemeProvider>
+                <Box maxW='600px' p='30px' border='1px solid red'>
+                    <CSSReset />
+                    <Form onSubmit={() => null}>{() => storyFn()}</Form>
+                </Box>
+            </ThemeProvider>
+        ),
+    ],
+}
+
+export const Default = () => (
+    <CardOptions
+        options={[{ value: 'x' }, { value: 'y' }, { value: 'z' }]}
+        name='sdf'
+    />
+)
+
+export const WithLabels = () => (
+    <CardOptions
+        options={[
+            { value: 'x', label: 'The x option is the first' },
+            { value: 'y', label: 'The y option' },
+            { value: 'z', label: 'The Z makes sleep' },
+        ]}
+        name='sdf'
+    />
+)
+
+export const WithSubLabels = () => (
+    <CardOptions
+        options={[
+            {
+                value: 'x',
+                label: 'The x option is the first',
+                sublabel: 'Sublabels add text no one reads',
+            },
+            {
+                value: 'y',
+                label: 'The y option',
+                sublabel: 'maybe true',
+            },
+            {
+                value: 'z',
+                label: 'The Z makes sleep',
+                sublabel: 'Sublabels add text no one reads',
+            },
+        ]}
+        name='sdf'
+    />
+)
+
+export const WithIcons = () => (
+    <CardOptions
+        options={[
+            {
+                icon: <FiAirplay />,
+                value: 'x',
+                label: 'The x option is the first',
+                sublabel: 'Sublabels add text no one reads',
+            },
+            {
+                icon: <FiAlertOctagon />,
+                value: 'y',
+                label: 'The y option',
+                sublabel: 'maybe true',
+            },
+            {
+                icon: <FiAnchor />,
+                value: 'z',
+                label: 'The Z makes sleep',
+                sublabel: 'Sublabels add text no one reads',
+            },
+        ]}
+        name='sdf'
+    />
+)
