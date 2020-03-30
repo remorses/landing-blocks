7ddb4b55b1b355ac9f80aef2894a0d63161fe166
remorses <beats.by.morse@gmail.com>
2020-03-30 17:29:44 +0200

    added dark prop

--- core/src/layout.tsx
+++ core/src/layout.tsx
@@ -1,6 +1,6 @@
-import { Flex, FlexProps, Box } from '@chakra-ui/core'
+import { Flex, FlexProps, Box, DarkMode } from '@chakra-ui/core'
 
-import React, { FC, forwardRef } from 'react'
+import React, { FC, forwardRef, Fragment } from 'react'
 
 export const Col: FC<FlexProps> = forwardRef((props, ref) => {
     return <Flex ref={ref} flexDir='column' {...props} />
@@ -14,29 +14,55 @@ export const Spacer = ({ x = '0px', y = '0px' }) => {
     return <Box mt={y} ml={x} />
 }
 
+const Dark = ({ children }) => {
+    return (
+        <DarkMode>
+            <Box
+                bg='gray.800'
+                color='whiteAlpha.900'
+                borderColor='whiteAlpha.300'
+                placeholder='whiteAlpha.400'
+            >
+                {children}
+            </Box>
+        </DarkMode>
+    )
+}
+
 export const PageContainer: FC<{
     floatingElement?: any
+    dark?: boolean
 } & FlexProps> = forwardRef(
-    ({ children, floatingElement = null, ...props }, ref) => {
+    ({ children, floatingElement = null, dark = false, ...props }, ref) => {
+        const Mode = dark ? Dark : Fragment
+        console.log({ Mode })
         return (
-            <Col position='relative' ref={ref} width='100%' align='center' {...props}>
+            <Mode>
                 <Col
-                    position='absolute'
+                    position='relative'
+                    ref={ref}
                     width='100%'
-                    top='0'
-                    left='0'
-                    right='0'
-                    bottom='0'
                     align='center'
-                    justify='center'
-                    overflow='hidden'
+                    {...props}
                 >
-                    {floatingElement}
+                    <Col
+                        position='absolute'
+                        width='100%'
+                        top='0'
+                        left='0'
+                        right='0'
+                        bottom='0'
+                        align='center'
+                        justify='center'
+                        overflow='hidden'
+                    >
+                        {floatingElement}
+                    </Col>
+                    <Col px='20px' w='100%' maxW='pageContainer'>
+                        {children}
+                    </Col>
                 </Col>
-                <Col px='20px' w='100%' maxW='pageContainer'>
-                    {children}
-                </Col>
-            </Col>
+            </Mode>
         )
     },
 )
