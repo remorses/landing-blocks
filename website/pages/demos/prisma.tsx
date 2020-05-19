a261e2412ea6543a8f5e3ff090854c7d7402625f
remorses <beats.by.morse@gmail.com>
2020-05-19 12:46:45 +0200

    website/p: added prisma globe

--- website/pages/demos/prisma.tsx
+++ website/pages/demos/prisma.tsx
@@ -46,7 +46,7 @@ const Page = () => (
             />
         </Head>
         <LandingProvider primary='#48BB79'>
-            <Stack spacing='60px' bg='#252D3B'>
+            <Stack  spacing='60px' position='relative' backgroundSize='cover' background="url('/prisma/globe.svg') center -300px no-repeat, radial-gradient( 37.86% 77.79% at 50% 100%, rgba(113,128,150,0.25) 0%, rgba(113,128,150,0) 100% ), linear-gradient(180deg,#1a202c 0%,#2d3748 100%), linear-gradient(180deg,#0d0f14 0%,rgba(27,32,43,0) 100%),#2f3747">
                 <NavBar
                     dark
                     logo={
