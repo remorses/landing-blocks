7cca0a6162f08eedca4a91b6a7ce15b43873b4c5
remorses <beats.by.morse@gmail.com>
2021-10-14 11:33:31 +0200

    landing-blocks/src/Link: forwardRef on link

--- landing-blocks/src/Link.tsx
+++ landing-blocks/src/Link.tsx
@@ -1,29 +1,35 @@
-import { Link as Link_, LinkProps as LinkProps_, useColorMode } from '@chakra-ui/react'
-import React from 'react'
+import {
+    Link as Link_,
+    LinkProps as LinkProps_,
+    useColorMode,
+} from '@chakra-ui/react'
+import React, { forwardRef } from 'react'
 
 export type LinkProps = LinkProps_
 
-export const Link = ({ children, ...props }: LinkProps) => {
-    const { colorMode } = useColorMode()
-    return (
-        <Link_
-            w='fit-content'
-            position='relative'
-            lineHeight='1.4em'
-            textDecoration='none'
-            // color='primary'
-            fontWeight='medium'
-            borderBottom='2px solid'
-            borderColor='transparent'
-            py='0.2em'
-            isTruncated
-            transition='all 0.2s ease-in-out 0s'
-            _hover={{
-                borderColor: 'currentColor',
-            }}
-            {...props}
-        >
-            {children}
-        </Link_>
-    )
-}
+export const Link = forwardRef<LinkProps, any>(
+    ({ children, ...props }, ref) => {
+        return (
+            <Link_
+                ref={ref}
+                w='fit-content'
+                position='relative'
+                lineHeight='1.4em'
+                textDecoration='none'
+                // color='primary'
+                fontWeight='medium'
+                borderBottom='2px solid'
+                borderColor='transparent'
+                py='0.2em'
+                isTruncated
+                transition='all 0.2s ease-in-out 0s'
+                _hover={{
+                    borderColor: 'currentColor',
+                }}
+                {...props}
+            >
+                {children}
+            </Link_>
+        )
+    },
+)
